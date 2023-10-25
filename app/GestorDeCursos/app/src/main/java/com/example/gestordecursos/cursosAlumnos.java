6ee05f5074e0937548667fed7bfa57c921f5d566
serrodgen <srodgen0910@g.educaand.es>
2023-10-25 20:31:05 +0200

    Created the cursosAlumnos screen

---
+++ app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/cursosAlumnos.java
@@ -0,0 +1,28 @@
+package com.example.gestordecursos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class cursosAlumnos extends AppCompatActivity {
+
+    FuncionesVarias fv = new FuncionesVarias();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cursos_alumnos);
+    }
+
+    /**
+     * Funcion para salir de la pantalla a la pantalla de LogIn
+     * @param v
+     */
+    public void salir(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
