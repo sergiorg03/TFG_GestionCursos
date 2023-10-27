0ea0870c75bb45f3b5c9c074b8bc14ad26f20a29
serrodgen <srodgen0910@g.educaand.es>
2023-10-27 22:11:28 +0200

    Changed some att in the DB, Done the api to get the courses

--- app/API/getCourses.php
+++ app/API/getCourses.php
@@ -11,53 +11,126 @@ $errores = [];
 $datos = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+    // Segunda consulta para obtener los datos de las preguntas de los tests
+    $SELECT2 = 'SELECT p.id AS id_pregunta, p.enunciado AS enunciado_preguntas, p.id_curso
+        FROM preguntas AS p
+        WHERE p.id_curso = :idCurso;';
+
+    // Tercera consulta para obtener las opciones de cada pregunta
+    $SELECT3 = 'SELECT r.id AS id_respuesta, r.opcion AS opcion_respuesta, r.esCorrecta AS esCorrecta_respuesta
+        FROM respuestasTest AS r
+        WHERE r.id_pregunta = :idPreg;';
+
     if (isset($_GET['id_curso'])) { // Está definido el id del curso a obtener
+        
+        // consulta para obtener el curso
+        $SELECT = 'SELECT c.id AS id_curso, c.nombre AS nombre_curso, c.ruta_pdf AS ruta_pdf_curso
+                        FROM cursos AS c
+                        WHERE c.id = :id_curso;';
 
+        $consulta = $conexion->prepare($SELECT);
+        $consulta->bindParam(':id_curso', $_GET['id_curso']);
+        $consulta->execute();
+
+        if ($consulta->rowCount() > 0) {
+            $cursoConTestyOpciones = [];
+
+            while ($datosCursos = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                $cursoConTestyOpciones[] = $datosCursos;
+
+                // Ejecutamos la consulta para obtener las preguntas de los test del curso correspondiente
+                $consulta2 = $conexion->prepare($SELECT2);
+                $consulta2->bindParam(':idCurso', $datosCursos['id_curso']);
+                $consulta2->execute();
+                
+                $preguntasTest = []; // Array para obtener los enunciados de las preguntas
+
+                while ($enunciado_opcion = $consulta2->fetch(PDO::FETCH_ASSOC)) {
+
+                    $preguntasTest[] = $enunciado_opcion; // Array para obtener los enunciados de las preguntas
+                    $consulta3 = $conexion->prepare($SELECT3);
+                    $consulta3->bindParam(':idPreg', $enunciado_opcion['id_pregunta']);
+                    $consulta3->execute();
+
+                        
+                    $opcionesTestPregunta = []; // Array para obtener los valores de las opciones del test
+                    while ($opcion = $consulta3->fetch(PDO::FETCH_ASSOC)) {
+                        $opcionesTestPregunta[] = $opcion;
+                    }
+                    $preguntasTest[] = $opcionesTestPregunta;   
+                }
+                $cursoConTestyOpciones[] = $preguntasTest;
+            }
+            header($headerJSON);
+            header($codigosHTTP['200']);
+            $jsonDatos = json_encode($cursoConTestyOpciones);
+
+        }else {
+            header($headerJSON);
+            header($codigosHTTP['404']);
+            $jsonDatos = json_encode(array('No exite el curso indicado. '));
+        }
+        
     } else { // NO está definida la id del curso
-        // Consulta de prueba
-        /*$SELECT = 'SELECT c.id AS `id_curso`, c.nombre AS `nombre_curso`, c.ruta_pdf AS `ruta_pdf_curso`, p.id AS `id_preguntas`, p.enunciado AS `enunciado_preguntas`, r.id AS `id_respuesta`, r.opcion AS `opcion_respuesta`, r.esCorrecta AS `esCorrecta_respuesta`
-                        FROM `gestionCursos.cursos` AS c
-                            INNER JOIN `gestionCursos.preguntas` AS p ON `c.id` = `p.id_curso`
-                            INNER JOIN `gestionCursos.respuestasTest` AS r ON `p.id` = `r.id_pregunta`;';*/
 
-        // Consulta para obtener los datos
+        // Consultas para obtener los datos
         // Primera consulta para obtener los datos de los cursos
         $SELECT = 'SELECT c.id AS id_curso, c.nombre AS nombre_curso, c.ruta_pdf AS ruta_pdf_curso
                         FROM cursos AS c;';
-        
-        // Segunda consulta para obtener los datos de las preguntas de los tests
-        $SELECT2 = 'SELECT p.id AS id_pregunta, p.enunciado AS enunciado_preguntas
-                        FROM preguntas AS p
-                        WHERE p.id_curso = :idCurso;';
 
-        // Tercera consulta para obtener las opciones de cada pregunta
-        $SELECT3 = 'SELECT r.id AS id_respuesta, r.opcion AS opcion_respuesta, r.esCorrecta AS esCorrecta_respuesta
-                        FROM respuestasTest AS r
-                        WHERE r.id_pregunta = :idPreg;';
-
+        // preparamos la primera consulta
         $consulta = $conexion->prepare($SELECT);
+        
+        // Ejecutamos la consulta
         $consulta->execute();
-        if ($consulta->rowCount() > 0) {
-            $resultado = [];
 
-            while ($datosCursos = $consulta->fetch(PDO::FETCH_ASSOC)) { // Finish
-                $resultado[] = $datosCursos;
+        if ($consulta->rowCount() > 0) {
+            // Creamos un array en el que guardaremos los cursos, las preguntas y sus opciones
+            $CursosConPreguntasyRespuestas = [];
+
+            while ($datosCursos = $consulta->fetch(PDO::FETCH_ASSOC)) { 
+                $CursosConPreguntasyRespuestas[] = $datosCursos; // Obtenemos los datos de los cursos
                 
-                /*
+                //print_r($CursosConPreguntasyRespuestas);
+                //echo $datosCursos['id_curso'];
+                
+                // Ejecutamos la consulta para obtener las preguntas de los test del curso correspondiente
                 $consulta2 = $conexion->prepare($SELECT2);
-                $consulta2->bindParam(':id_pregunta', $resultado['id_pregunta']);
+                $consulta2->bindParam(':idCurso', $datosCursos['id_curso']);
                 $consulta2->execute();
-                if ($consulta2->rowCount() > 0) {
-                    $opcinesTests = [];
-                    while ($enunciado_opcion = $consulta->fetch(PDO::FETCH_ASSOC)) {
-                        $opcinesTests[] = $enunciado_opcion;
+                
+                $preguntasTest = []; // Array para obtener los enunciados de las preguntas
+
+                while ($enunciado_opcion = $consulta2->fetch(PDO::FETCH_ASSOC)) {
+
+                    $preguntasTest[] = $enunciado_opcion; // Array para obtener los enunciados de las preguntas
+                    $consulta3 = $conexion->prepare($SELECT3);
+                    $consulta3->bindParam(':idPreg', $enunciado_opcion['id_pregunta']);
+                    $consulta3->execute();
+
+                        
+                    $opcionesTestPregunta = []; // Array para obtener los valores de las opciones del test
+                    while ($opcion = $consulta3->fetch(PDO::FETCH_ASSOC)) {
+                        $opcionesTestPregunta[] = $opcion;
                     }
-                    $resultado['opcines_test'] = $opcinesTests;
-                }*/
-            }print_r($resultado);
+                    $preguntasTest[] = $opcionesTestPregunta;
+                        
+                }
+                $CursosConPreguntasyRespuestas[] = $preguntasTest;
+                
+            }// echo "<br/><br/><br/><br/>";print_r($CursosConPreguntasyRespuestas);
+            header($headerJSON);
+            header($codigosHTTP['200']);
+            $jsonDatos = json_encode($CursosConPreguntasyRespuestas);
         }
     }
-}
+    echo $jsonDatos;
 
+    $consulta3 = null;
+    $consulta2 = null;
+    $consulta = null;
+    $conexion = null;
+}
 die();
 ?>
