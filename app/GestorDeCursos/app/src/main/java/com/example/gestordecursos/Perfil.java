bcbb6e768b712d499c79b82fe14c2b6e87c1f40f
Sergio Rodriguez <srodgen0910@gmail.com>
2023-11-03 13:24:17 +0100

    Done the screen perfil

--- app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/Perfil.java
+++ app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/Perfil.java
@@ -2,7 +2,12 @@ package com.example.gestordecursos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  *
@@ -11,9 +16,43 @@ import android.os.Bundle;
  */
 public class Perfil extends AppCompatActivity {
 
+    Class claseAnterior;
+    String dni;
+    TextView tv_dni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        dni = getIntent().getStringExtra("dni");
+        tv_dni = findViewById(R.id.dni_perfil);
+        tv_dni.setText(dni.toUpperCase());
+
+        mostrarDatos();
+    }
+
+    /**
+     * Metodo para volver a la clase anterior
+     * @param v
+     */
+    public void volver(View v){
+        Intent i = new Intent(this, claseAnterior);
+        startActivity(i);
+        finish();
+    }
+
+    /**
+     * Método para modificar el perfil
+     * @param v
+     */
+    public void updateProfile(View v){
+
+    }
+
+    /**
+     * Metodo para mostrar los datos del usuario
+     */
+    public void mostrarDatos(){
+
     }
 }
