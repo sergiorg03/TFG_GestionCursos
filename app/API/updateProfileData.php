f9cea56cff40eab39ec683408bfe43ffaa1d2285
Sergio Rodriguez <srodgen0910@gmail.com>
2023-11-03 17:25:23 +0100

    Done the api to update  profiles, added a new function to check if a string is a md5 or not

---
+++ app/API/updateProfileData.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * 
+ * Archivo para modificar los datos del usuario
+ * 
+ */
+require_once("./utilities/conexion.php");
+require_once("./utilities/funciones.php");
+$conexion = conectarPDO($database);
+$errores = [];
+$datos = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    // Se transforma el json de entrada
+    $datos = json_decode(file_get_contents('php://input'), true);
+    //Obtenemos el id del dato a modificar
+    $dni = $datos['dni'];
+    //Actualizamos el dato
+    $update = 'UPDATE personas
+                    SET nombre = :nombre, 
+                        apellido1 = :apellido1, 
+                        apellido2 = :apellido2, 
+                        telefono = :telefono,
+                        email = :email, 
+                        contra = :contra
+                    WHERE dni = :dni;';
+    // Preparamos la consulta
+    $consulta = $conexion->prepare($update);
+    //Asignamos los valores
+    $consulta->bindParam(':nombre', $datos['nombre']);
+    $consulta->bindParam(':apellido1', $datos['apellido1']);
+    $consulta->bindParam(':apellido2', $datos['apellido2']);
+    $consulta->bindParam(':telefono', $datos['telefono']);
+    $consulta->bindParam(':email', $datos['email']);
+    $contra = isMd5($datos['contra'])? $datos['contra'] : cadToMD5($datos['contra']);
+    $consulta->bindParam(':contra', $contra);
+
+    $consulta->bindParam(':dni', $dni);
+    //Comprobamos que no haya ningun error en la ejecucion
+    try {
+        //Ejecutamos la consulta
+        $consulta->execute();
+        //Creacion del JSON con los datos
+        $jsonDatos = json_encode($datos);
+        //Devolvemos mensaje de que todo esta correcto
+        header($headerJSON);
+        header($codigosHTTP['200']);
+    } catch (PDOException $exception) { // Se produce error
+// Capturamos el error producido
+        $errorDescription = $exception->getMessage();
+        $datos = array('error' => $errorDescription);
+        // Creamos JSON con datos recibidos y el nuevo id
+        $jsonDatos = json_encode($datos);
+        // Cabeceras de error en la ejecución de la consulta
+        header($headerJSON);
+        header($codigosHTTP['500']);
+    }
+    // Devolvemos JSON
+    echo $jsonDatos;
+
+    //Cerramos la consulta, la conexion y salimos del programa
+    $consulta = null;
+    $conexion = null;
+
+    die();
+}
+?>
--- app/API/utilities/funciones.php
+++ app/API/utilities/funciones.php
@@ -10,5 +10,15 @@
         return md5($cadena);
     }
 
-    
+    /**
+     * Funcion que comprueba si una cadena es un hash MD% o no
+     * $md5 {String} -- Cadena a comrpobar si es un hash MD%
+     * return {boolean} -- True si la cadena introducida es un HASH MD5, flase si no lo es
+     */
+    function isMd5($md5): bool
+    {
+        
+        return preg_match('/^[a-f0-9A-F]{32}$/', $md5);
+        
+    }
 ?>
