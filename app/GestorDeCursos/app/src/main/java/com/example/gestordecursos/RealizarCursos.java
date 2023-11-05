54ecf119b9d77013ef84a8bb02dede92876845e7
serrodgen <srodgen0910@g.educaand.es>
2023-11-05 13:38:21 +0100

    Done the cargarPreguntas() Method

--- app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/RealizarCursos.java
+++ app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/RealizarCursos.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,7 +32,56 @@ public class RealizarCursos extends AppCompatActivity {
     String nombreClase;
     Class claseAnterior;
     String id_curso;
-
+    TextView pregunta1;
+    TextView pregunta2;
+    TextView pregunta3;
+    TextView pregunta4;
+    TextView pregunta5;
+    TextView pregunta6;
+    TextView pregunta7;
+    TextView pregunta8;
+    TextView pregunta9;
+    TextView pregunta10;
+    // Opciones pregunta 1
+    RadioButton op1_p1;
+    RadioButton op2_p1;
+    RadioButton op3_p1;
+    // Opciones pregunta 2
+    RadioButton op1_p2;
+    RadioButton op2_p2;
+    RadioButton op3_p2;
+    // Opciones pregunta 3
+    RadioButton op1_p3;
+    RadioButton op2_p3;
+    RadioButton op3_p3;
+    // Opciones pregunta 4
+    RadioButton op1_p4;
+    RadioButton op2_p4;
+    RadioButton op3_p4;
+    // Opciones pregunta 5
+    RadioButton op1_p5;
+    RadioButton op2_p5;
+    RadioButton op3_p5;
+    // Opciones pregunta 6
+    RadioButton op1_p6;
+    RadioButton op2_p6;
+    RadioButton op3_p6;
+    // Opciones pregunta 7
+    RadioButton op1_p7;
+    RadioButton op2_p7;
+    RadioButton op3_p7;
+    // Opciones pregunta 8
+    RadioButton op1_p8;
+    RadioButton op2_p8;
+    RadioButton op3_p8;
+    // Opciones pregunta 9
+    RadioButton op1_p9;
+    RadioButton op2_p9;
+    RadioButton op3_p9;
+    // Opciones pregunta 10
+    RadioButton op1_p10;
+    RadioButton op2_p10;
+    RadioButton op3_p10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +93,51 @@ public class RealizarCursos extends AppCompatActivity {
         claseAnterior = fv.obtenerClase(nombreClase);
         id_curso = getIntent().getStringExtra("idCurso");
 
+        // Asignamos los TextView de la pregunta a su recurso correspodiente
+        pregunta1   = findViewById(R.id.pregunta1);
+        pregunta2   = findViewById(R.id.pregunta2);
+        pregunta3   = findViewById(R.id.pregunta3);
+        pregunta4   = findViewById(R.id.pregunta4);
+        pregunta5   = findViewById(R.id.pregunta5);
+        pregunta6   = findViewById(R.id.pregunta6);
+        pregunta7   = findViewById(R.id.pregunta7);
+        pregunta8   = findViewById(R.id.pregunta8);
+        pregunta9   = findViewById(R.id.pregunta9);
+        pregunta10  = findViewById(R.id.pregunta10);
+
+        // Asignamos los RadioButton a su recurso correspondiente
+        op1_p1  = findViewById(R.id.op1_p1);
+        op2_p1  = findViewById(R.id.op2_p1);
+        op3_p1  = findViewById(R.id.op3_p1);
+        op1_p2  = findViewById(R.id.op1_p2);
+        op2_p2  = findViewById(R.id.op2_p2);
+        op3_p2  = findViewById(R.id.op3_p2);
+        op1_p3  = findViewById(R.id.op1_p3);
+        op2_p3  = findViewById(R.id.op2_p3);
+        op3_p3  = findViewById(R.id.op3_p3);
+        op1_p4  = findViewById(R.id.op1_p4);
+        op2_p4  = findViewById(R.id.op2_p4);
+        op3_p4  = findViewById(R.id.op3_p4);
+        op1_p5  = findViewById(R.id.op1_p5);
+        op2_p5  = findViewById(R.id.op2_p5);
+        op3_p5  = findViewById(R.id.op3_p5);
+        op1_p6  = findViewById(R.id.op1_p6);
+        op2_p6  = findViewById(R.id.op2_p6);
+        op3_p6  = findViewById(R.id.op3_p6);
+        op1_p7  = findViewById(R.id.op1_p7);
+        op2_p7  = findViewById(R.id.op2_p7);
+        op3_p7  = findViewById(R.id.op3_p7);
+        op1_p8  = findViewById(R.id.op1_p8);
+        op2_p8  = findViewById(R.id.op2_p8);
+        op3_p8  = findViewById(R.id.op3_p8);
+        op1_p9  = findViewById(R.id.op1_p9);
+        op2_p9  = findViewById(R.id.op2_p9);
+        op3_p9  = findViewById(R.id.op3_p9);
+        op1_p10 = findViewById(R.id.op1_p10);
+        op2_p10 = findViewById(R.id.op2_p10);
+        op3_p10 = findViewById(R.id.op3_p10);
+
+        // Mostramos las preguntas
         cargarPreguntas();
     }
 
@@ -72,58 +168,112 @@ public class RealizarCursos extends AppCompatActivity {
             @Override
             public void onConsultaExitosa(Map<String[], List<String[]>> preguntas) {
 
+                for (String[] test: preguntas.keySet()) {
+                    System.out.println("Clave: " + test[0]+test[1] + " Valor: " + preguntas.get(test));
+
+                    int id_preg = Integer.parseInt(test[0]);
+                    String enunciado_preg = test[1];
+                    System.out.println("Pregunta: ");
+                    System.out.println("id--> "+ id_preg);
+                    System.out.println("enunciado--> "+ enunciado_preg);
+
+                    System.out.println("Opciones: ");
+                    // Opcion 1
+                    System.out.println("Opcion 1-->");
+                    String id_op1 = preguntas.get(test).get(0)[0];
+                    String enun_op1 = preguntas.get(test).get(0)[1];
+                    String esCorrecta_op1 = preguntas.get(test).get(0)[2];
+                    System.out.println("id_op1-->"+ id_op1);
+                    System.out.println("enun_op1-->"+ enun_op1);
+                    System.out.println("esCorrecta_op1-->"+ esCorrecta_op1);
+                    // Opcion 2
+                    System.out.println("Opcion 2-->");
+                    String id_op2 = preguntas.get(test).get(1)[0];
+                    String enun_op2 = preguntas.get(test).get(1)[1];
+                    String esCorrecta_op2 = preguntas.get(test).get(1)[2];
+                    System.out.println("id_op2-->"+ id_op2);
+                    System.out.println("enun_op2-->"+ enun_op2);
+                    System.out.println("esCorrecta_op2-->"+ esCorrecta_op2);
+                    // Opcion 3
+                    System.out.println("Opcion 3-->");
+                    String id_op3 = preguntas.get(test).get(2)[0];
+                    String enun_op3 = preguntas.get(test).get(2)[1];
+                    String esCorrecta_op3 = preguntas.get(test).get(2)[2];
+                    System.out.println("id_op3-->"+ id_op3);
+                    System.out.println("enun_op3-->"+ enun_op3);
+                    System.out.println("esCorrecta_op3-->"+ esCorrecta_op3);
+
+                    switch (id_preg){
+                        case 1:
+                            pregunta1.setText(enunciado_preg);
+                            op1_p1.setText(enun_op1);
+                            op2_p1.setText(enun_op2);
+                            op3_p1.setText(enun_op3);
+                            break;
+                        case 2:
+                            pregunta2.setText(enunciado_preg);
+                            op1_p2.setText(enun_op1);
+                            op2_p2.setText(enun_op2);
+                            op3_p2.setText(enun_op3);
+                            break;
+                        case 3:
+                            pregunta3.setText(enunciado_preg);
+                            op1_p3.setText(enun_op1);
+                            op2_p3.setText(enun_op2);
+                            op3_p3.setText(enun_op3);
+                            break;
+                        case 4:
+                            pregunta4.setText(enunciado_preg);
+                            op1_p4.setText(enun_op1);
+                            op2_p4.setText(enun_op2);
+                            op3_p4.setText(enun_op3);
+                            break;
+                        case 5:
+                            pregunta5.setText(enunciado_preg);
+                            op1_p5.setText(enun_op1);
+                            op2_p5.setText(enun_op2);
+                            op3_p5.setText(enun_op3);
+                            break;
+                        case 6:
+                            pregunta6.setText(enunciado_preg);
+                            op1_p6.setText(enun_op1);
+                            op2_p6.setText(enun_op2);
+                            op3_p6.setText(enun_op3);
+                            break;
+                        case 7:
+                            pregunta7.setText(enunciado_preg);
+                            op1_p7.setText(enun_op1);
+                            op2_p7.setText(enun_op2);
+                            op3_p7.setText(enun_op3);
+                            break;
+                        case 8:
+                            pregunta8.setText(enunciado_preg);
+                            op1_p8.setText(enun_op1);
+                            op2_p8.setText(enun_op2);
+                            op3_p8.setText(enun_op3);
+                            break;
+                        case 9:
+                            pregunta9.setText(enunciado_preg);
+                            op1_p9.setText(enun_op1);
+                            op2_p9.setText(enun_op2);
+                            op3_p9.setText(enun_op3);
+                            break;
+                        case 10:
+                            pregunta10.setText(enunciado_preg);
+                            op1_p10.setText(enun_op1);
+                            op2_p10.setText(enun_op2);
+                            op3_p10.setText(enun_op3);
+                            break;
+                    }
+                }
             }
 
             @Override
             public void onConsultaError(VolleyError e) {
-
+                fv.mostrarMensaje(RealizarCursos.this, "No se pudieron obtener tests para este curso. ");
             }
         });
     }
-    /*public void mostrarTodosCursos(){
-
-        List<String []> cursos = getCursos(new cursosAlumnos.ConsultarDatos() {
-
-            @Override
-            public void onConsultaExitosa(List<String[]> listaCursos) {
-
-                Map<String, String[]> todoDatosCurso = new HashMap<>();
-                int i = 1;
-                // Añadimos los cursos con sus respectivas notas
-                for (String[] curso : listaCursos) {
-
-                    String [] datosCurso = new String[]{curso[1], curso[2]};
-                    todoDatosCurso.put(curso[0], datosCurso);
-                }
-
-                for (Map.Entry<String, String[]> entry : todoDatosCurso.entrySet()) {
-                    String key = entry.getKey();
-                    switch (i) {
-                        case 1:
-                            id_curso1 = key;
-                            tv_curso1.setText(todoDatosCurso.get(key)[0]);
-                            ruta_pdf_curso1 = todoDatosCurso.get(key)[1];
-                            System.out.println("Id: "+ id_curso1);
-                            System.out.println("Nombre curso: "+ tv_curso1.getText().toString());
-                            System.out.println("ruta: "+ ruta_pdf_curso1);
-                            break;
-                        case 2:
-                            id_curso2 = key;
-                            tv_curso2.setText(todoDatosCurso.get(key)[0]);
-                            ruta_pdf_curso2 = todoDatosCurso.get(key)[1];
-                            break;
-
-                    }
-                    i++;
-                }
-            }
-
-            @Override
-            public void onConsultaError(VolleyError ve) {
-                fv.mostrarMensaje(cursosAlumnos.this, "No se pudieron recopilar datos. ");
-            }
-        });
-    }*/
 
     public Map<String[], List<String[]>> getPreguntas(ConsultarDatos cd){
 
@@ -150,6 +300,17 @@ public class RealizarCursos extends AppCompatActivity {
 
                                     pregunta[0] = json.getString("id_pregunta");
                                     pregunta[1] = json.getString("enunciado_preguntas");
+                                    /*
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println("------------------ TRAZAS ----------------");
+                                    System.out.println("id_pregunta--> "+pregunta[0]);
+                                    System.out.println("enunciado_pregunta--> "+pregunta[1]);
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                     */
                                 }
 
                                 List<String[]> Lista_opciones = new ArrayList<>();
@@ -160,6 +321,18 @@ public class RealizarCursos extends AppCompatActivity {
                                     opciones[1] = json.getString("opcion_respuesta");
                                     opciones[2] = json.getString("esCorrecta_respuesta");
                                     Lista_opciones.add(opciones);
+                                    /*
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println("------------------ TRAZAS ----------------");
+                                    System.out.println("id_respuesta--> "+opciones[0]);
+                                    System.out.println("opcionRespuesta--> "+opciones[1]);
+                                    System.out.println("esCorrecta--> "+opciones[2]);
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    System.out.println(" ");
+                                    */
                                 }
                                 test.put(pregunta, Lista_opciones);
                             }
