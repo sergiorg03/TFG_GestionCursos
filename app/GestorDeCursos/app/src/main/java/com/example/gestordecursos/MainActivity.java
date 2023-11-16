2a90693215a4f675c9d580d475cc71b14b43dcde
sergiorg03 <sergiorodriguezprofesional@gmail.com>
2023-11-16 10:19:40 +0100

    Added a validation for the permits

--- app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/MainActivity.java
+++ app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/MainActivity.java
@@ -7,8 +7,14 @@ package com.example.gestordecursos;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -38,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
 
         usuario = findViewById(R.id.user);
         contra = findViewById(R.id.pass);
+/*
+        int concedidoPermisoEscritura = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int concedidoPermisoLectura = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (concedidoPermisoEscritura == PackageManager.PERMISSION_GRANTED && concedidoPermisoLectura == PackageManager.PERMISSION_GRANTED){
+            fv.mostrarMensaje(this, "Permiso concedido. ");
+        }else{
+            fv.mostrarMensaje(this, "No tiene permisos");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }*/
     }
 
     public void pantallaInfo(View view) {
