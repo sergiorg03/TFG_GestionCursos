e735d333b966a3e83af70408183c546b3b9a5c92
s.rod.gen <s.rodriguez.geniz@accenture.com>
2023-11-17 14:03:18 +0100

    Need to be corrected the script

--- script/DML.sql
+++ script/DML.sql
@@ -21,112 +21,112 @@ INSERT INTO gestionCursos.cursos (id, nombre, ruta_pdf)
 -- Pregunta 1
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado) 
 	VALUES
-		(1, (SELECT LAST_INSERT_ID()), '¿Cuál comando se utiliza para mostrar el contenido de un directorio?');
+		(1, (LAST_INSERT_ID()), '¿Cuál comando se utiliza para mostrar el contenido de un directorio?');
 
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 1, (SELECT LAST_INSERT_ID()), 'ls', 1),
-		(2, 1, (SELECT LAST_INSERT_ID()), 'cp', 0),
-		(3, 1, (SELECT LAST_INSERT_ID()), 'cd', 0);
+		(1, 1, (LAST_INSERT_ID()), 'ls', 1),
+		(2, 1, (LAST_INSERT_ID()), 'cp', 0),
+		(3, 1, (LAST_INSERT_ID()), 'cd', 0);
 
 -- Pregunta 2
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(2, (SELECT LAST_INSERT_ID()), '¿Qué ruta se corresponde con una ruta absoluta?');
+		(2, (LAST_INSERT_ID()), '¿Qué ruta se corresponde con una ruta absoluta?');
 
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 2, (SELECT LAST_INSERT_ID()), './documentos/file.txt', 0),
-		(2, 2, (SELECT LAST_INSERT_ID()), '/home/usuario/documentos/file.txt', 1),
-		(3, 2, (SELECT LAST_INSERT_ID()), 'ninguna es correcta', 0);
+		(1, 2, (LAST_INSERT_ID()), './documentos/file.txt', 0),
+		(2, 2, (LAST_INSERT_ID()), '/home/usuario/documentos/file.txt', 1),
+		(3, 2, (LAST_INSERT_ID()), 'ninguna es correcta', 0);
 		
 -- Pregunta 3
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(3, (SELECT LAST_INSERT_ID()), '¿Qué comando se usa para crear un directorio?');
+		(3, (LAST_INSERT_ID()), '¿Qué comando se usa para crear un directorio?');
 
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 3, (SELECT LAST_INSERT_ID()), 'mkdir', 1),
-		(2, 3, (SELECT LAST_INSERT_ID()), 'touch', 0),
-		(3, 3, (SELECT LAST_INSERT_ID()), 'cat', 0);
+		(1, 3, (LAST_INSERT_ID()), 'mkdir', 1),
+		(2, 3, (LAST_INSERT_ID()), 'touch', 0),
+		(3, 3, (LAST_INSERT_ID()), 'cat', 0);
 		
 -- Pregunta 4
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(4, (SELECT LAST_INSERT_ID()), '¿Cuál comando se utiliza para eliminar un archivo?');
+		(4, (LAST_INSERT_ID()), '¿Cuál comando se utiliza para eliminar un archivo?');
 
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 4, (SELECT LAST_INSERT_ID()), 'rm', 1),
-		(2, 4, (SELECT LAST_INSERT_ID()), 'rd', 0),
-		(3, 4, (SELECT LAST_INSERT_ID()), 'del', 0);
+		(1, 4, (LAST_INSERT_ID()), 'rm', 1),
+		(2, 4, (LAST_INSERT_ID()), 'rd', 0),
+		(3, 4, (LAST_INSERT_ID()), 'del', 0);
 
 -- Pregunta 5
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(5, (SELECT LAST_INSERT_ID()), '¿Qué comando se utiliza para buscar archivos en un directorio?');
+		(5, (LAST_INSERT_ID()), '¿Qué comando se utiliza para buscar archivos en un directorio?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 5, (SELECT LAST_INSERT_ID()), 'locate', 0),
-		(2, 5, (SELECT LAST_INSERT_ID()), 'find', 1),
-		(3, 5, (SELECT LAST_INSERT_ID()), 'search', 0);
+		(1, 5, (LAST_INSERT_ID()), 'locate', 0),
+		(2, 5, (LAST_INSERT_ID()), 'find', 1),
+		(3, 5, (LAST_INSERT_ID()), 'search', 0);
 
 -- Pregunta 6
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(6, (SELECT LAST_INSERT_ID()), '¿Qué comando se usa ir al directorio raiz?');
+		(6, (LAST_INSERT_ID()), '¿Qué comando se usa ir al directorio raiz?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 6, (SELECT LAST_INSERT_ID()), 'cd c:', 0),
-		(2, 6, (SELECT LAST_INSERT_ID()), '/', 0),
-		(3, 6, (SELECT LAST_INSERT_ID()), 'cd /', 1);
+		(1, 6, (LAST_INSERT_ID()), 'cd c:', 0),
+		(2, 6, (LAST_INSERT_ID()), '/', 0),
+		(3, 6, (LAST_INSERT_ID()), 'cd /', 1);
 
 -- Pregunta 7
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(7, (SELECT LAST_INSERT_ID()), '¿Qué comando se utiliza para cambiar el directorio actual?');
+		(7, (LAST_INSERT_ID()), '¿Qué comando se utiliza para cambiar el directorio actual?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 7, (SELECT LAST_INSERT_ID()), 'change', 0),
-		(2, 7, (SELECT LAST_INSERT_ID()), 'cd', 1),
-		(3, 7, (SELECT LAST_INSERT_ID()), 'switchdir', 0);
+		(1, 7, (LAST_INSERT_ID()), 'change', 0),
+		(2, 7, (LAST_INSERT_ID()), 'cd', 1),
+		(3, 7, (LAST_INSERT_ID()), 'switchdir', 0);
 
 -- Pregunta 8
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(8, (SELECT LAST_INSERT_ID()), '¿Cuál comando se utiliza para crear una copia de seguridad?');
+		(8, (LAST_INSERT_ID()), '¿Cuál comando se utiliza para crear una copia de seguridad?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 8, (SELECT LAST_INSERT_ID()), 'duplicity', 1),
-		(2, 8, (SELECT LAST_INSERT_ID()), 'BackUp', 0),
-		(3, 8, (SELECT LAST_INSERT_ID()), 'save', 0);
+		(1, 8, (LAST_INSERT_ID()), 'duplicity', 1),
+		(2, 8, (LAST_INSERT_ID()), 'BackUp', 0),
+		(3, 8, (LAST_INSERT_ID()), 'save', 0);
 
 -- Pregunta 9
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(9, (SELECT LAST_INSERT_ID()), '¿Qué comando se usa para mostrar el contenido de archivos?');
+		(9, (LAST_INSERT_ID()), '¿Qué comando se usa para mostrar el contenido de archivos?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 9, (SELECT LAST_INSERT_ID()), 'cat', 0),
-		(2, 9, (SELECT LAST_INSERT_ID()), 'more', 0),
-		(3, 9, (SELECT LAST_INSERT_ID()), 'Ambas son correctas', 1);
+		(1, 9, (LAST_INSERT_ID()), 'cat', 0),
+		(2, 9, (LAST_INSERT_ID()), 'more', 0),
+		(3, 9, (LAST_INSERT_ID()), 'Ambas son correctas', 1);
 
 -- Pregunta 10
 INSERT INTO gestionCursos.preguntas(id, id_curso, Enunciado)
 	VALUES
-		(10, (SELECT LAST_INSERT_ID()), '¿Cuál comando se utiliza para copiar archivos y directorios?');
+		(10, (LAST_INSERT_ID()), '¿Cuál comando se utiliza para copiar archivos y directorios?');
 		
 INSERT INTO gestionCursos.respuestasTest(id, id_pregunta, id_curso, opcion, esCorrecta) 
 	VALUES
-		(1, 10, (SELECT LAST_INSERT_ID()), 'cop', 0),
-		(2, 10, (SELECT LAST_INSERT_ID()), 'copyDir', 0),
-		(3, 10, (SELECT LAST_INSERT_ID()), 'cp', 1);
+		(1, 10, (LAST_INSERT_ID()), 'cop', 0),
+		(2, 10, (LAST_INSERT_ID()), 'copyDir', 0),
+		(3, 10, (LAST_INSERT_ID()), 'cp', 1);
 		
 -- ----------------------------------------
 -- Curso Bases de datos: Data Manipulation Languaje
