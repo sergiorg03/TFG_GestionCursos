011133ff15fa426d8a139ba748a7f153fb86ff38
srodgen0910 <srodgen0910@g.educaand.es>
2023-11-18 16:38:40 +0100

    Changed the profile of a person and added to gestionar table

--- script/DML.sql
+++ script/DML.sql
@@ -7,7 +7,7 @@ INSERT INTO gestionCursos.personas (dni, nombre, apellido1, apellido2, telefono,
 	VALUES
 		('33387392V', 'Juan',   'Lopez',   'Perez',     '123456789', 'juan@gmail.com',   'jualopper', (SELECT MD5('juan') FROM DUAL),   'a'),
 		('97939374S', 'Ana',    'Gomez',   'Martinez',  '987654321', 'ana@hotmail.com',  'angomart',  (SELECT MD5('ana') FROM DUAL),    'g'),
-		('32097880T', 'Ana',    'Romero',   'Raposo',  '987654321', 'anita@gmail.com',  'anapiquitos',  (SELECT MD5('piquitos') FROM DUAL),    'a'),
+		('32097880T', 'Ana',    'Romero',   'Raposo',  '987654321', 'anita@gmail.com',  'anapiquitos',  (SELECT MD5('piquitos') FROM DUAL),    'g'),
 		('29157459Z', 'Carlos', 'Sanchez', 'Rodriguez', '555555555', 'carlos@yahoo.com', 'carsanrod', (SELECT MD5('carlos') FROM DUAL), 'a');
 
 -- Tabla cursos
@@ -28,7 +28,7 @@ INSERT INTO gestionCursos.realizar (dni, id_curso, puntuacion)
 INSERT INTO gestionCursos.gestionar (dni, id_curso, last_updateDate)
 	VALUES
 		('97939374S', 3, SYSDATE()),
-		('97939374S', 2, SYSDATE()),
+		('32097880T', 2, SYSDATE()),
 		('97939374S', 1, SYSDATE());
 
 -- ----------------------------------------
