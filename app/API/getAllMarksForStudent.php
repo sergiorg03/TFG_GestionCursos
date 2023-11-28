d6aba455003ac85804c846a152c0d47521a51164
Sergio Rodriguez <srodgen0910@gmail.com>
2023-11-28 23:18:17 +0100

    Created the new file for the API to get all marks for a student, done the screen for showing the marks and started to do the logic

---
+++ app/API/getAllMarksForStudent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Archivo creado para la obtencion de todas las notas de los alumnos
+ */
+require_once("./utilities/conexion.php");
+require_once("./utilities/funciones.php");
+$conexion = conectarPDO($database);
+$errores = [];
+$datos = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    
+}
+
+die();
+?>
--- app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/pantallaNotasCursos.java
+++ app/GestorDeCursos/app/src/main/java/com/example/gestordecursos/pantallaNotasCursos.java
@@ -6,6 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Clase para mostrar las notas obtenidas por un alumno
  */
@@ -13,6 +27,7 @@ public class pantallaNotasCursos extends AppCompatActivity {
 
     FuncionesVarias fv = new FuncionesVarias();
     String dni;
+    String curso;
     String clase;
     Class claseAnterior;
 
@@ -22,8 +37,21 @@ public class pantallaNotasCursos extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_notas_cursos);
 
         dni = getIntent().getStringExtra("dni");
+        curso = getIntent().getStringExtra("curso");
         clase = getIntent().getStringExtra("clase");
         claseAnterior = fv.obtenerClase(clase);
+
+        iniciarVars();
+    }
+
+    /**
+     * Metodo para inicializar las variables
+     */
+    public void iniciarVars(){
+        // textviews
+
+
+        obtenerDatos();
     }
 
     /**
@@ -49,5 +77,64 @@ public class pantallaNotasCursos extends AppCompatActivity {
         finish();
     }
 
+    public interface ConsultarDatos{
+        void onConsultaExitosa();
+        void onConsultaError(VolleyError e);
+    }
 
+    /**
+     * Metodo que muestra las notas de los alumnos
+     */
+    public List<String[]> mostrarDatos(ConsultarDatos cd){
+        final String URL = fv.getURL()+"getAllMarksForStudent.php?dni="+dni.trim()+"&id_curso="+curso.trim();
+
+        RequestQueue rq = Volley.newRequestQueue(this);
+
+        List<String[]> lista = new ArrayList<>();
+
+        StringRequest sr = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray ja = new JSONArray(response);
+
+                            for (int i = 0; i < ja.length(); i++) {
+
+                            }
+                            /**
+                             *
+                             * TERMINAR
+                             *
+                             */
+                            cd.onConsultaExitosa();
+                        }catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                cd.onConsultaError(error);
+            }
+        });
+
+        rq.add(sr);
+        return lista;
+    }
+
+    public void obtenerDatos(){
+        List<String[]> listaNotas = mostrarDatos(new ConsultarDatos() {
+            @Override
+            public void onConsultaExitosa() {
+                
+            }
+
+            @Override
+            public void onConsultaError(VolleyError e) {
+                fv.mostrarMensaje(pantallaNotasCursos.this, "No se pudieron obtener datos. ");
+                e.printStackTrace();
+            }
+        });
+    }
 }
